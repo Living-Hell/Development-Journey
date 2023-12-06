6b1c385fef68ffd362608a089207a046948eb188
Shushant Kumar <s.living.hell@gmail.com>
2023-12-06 17:28:10 +0530

    Adding 2620 Counter

---
+++ JavaScript/30 Days of JS (Leetcode)/2620. Counter.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+//M1
+var createCounter = function (n) {
+  return () => n++;
+};
+
+//M2
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
