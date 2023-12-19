373a39ca2cfc2cdc05e5739fa7e34791d2e5ba62
Shushant Kumar <s.living.hell@gmail.com>
2023-12-19 10:05:29 +0530

    Adding 2625 in JavaScript

---
+++ JavaScript/30 Days of JS (Leetcode)/2625. Flatten Deeply Nested Array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+  if (n == 0) return arr.slice();
+
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      const sub = flat(arr[i], n - 1);
+      res.push(...sub);
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+};
