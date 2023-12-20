35b1736bcd0927d9cd461d7d2f848a0de2e2a21c
Shushant Kumar <s.living.hell@gmail.com>
2023-12-20 20:18:24 +0530

    Adding 2694 in JavaScript

---
+++ JavaScript/30 Days of JS (Leetcode)/2694. Event Emitter.js
@@ -0,0 +1,47 @@
+class EventEmitter {
+  constructor() {
+    this.eventMap = new Map();
+  }
+
+  /**
+   * @param {string} eventName
+   * @param {Function} callback
+   * @return {Object}
+   */
+  subscribe(eventName, callback) {
+    if (!this.eventMap.hasOwnProperty(eventName)) {
+      this.eventMap[eventName] = new Set();
+    }
+    this.eventMap[eventName].add(callback);
+    return {
+      unsubscribe: () => {
+        this.eventMap[eventName].delete(callback);
+      },
+    };
+  }
+
+  /**
+   * @param {string} eventName
+   * @param {Array} args
+   * @return {Array}
+   */
+  emit(eventName, args = []) {
+    let ans = [];
+    (this.eventMap[eventName] ?? []).forEach((callback) =>
+      ans.push(callback(...args))
+    );
+    return ans;
+  }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
