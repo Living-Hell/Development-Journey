d3e773f5c1df1b21014140f030aff756d3d178f0
Shushant Kumar <s.living.hell@gmail.com>
2023-12-24 11:40:55 +0530

    Fixed minor bugs in Javascript/Blacjack

--- JavaScript/Blackjack/index.js
+++ JavaScript/Blackjack/index.js
@@ -3,6 +3,15 @@ let chipsInput = document.getElementById("input-chips");
 
 function enterGame() {
   console.log(nameInput.value);
+  if (
+    nameInput.value === "" ||
+    chipsInput.value === "" ||
+    isNaN(chipsInput.value)
+  ) {
+    alert("Please fill in all fields correctly");
+    return;
+  }
   localStorage.setItem("name", nameInput.value);
   localStorage.setItem("chips", chipsInput.value);
+  window.location.href = "./game.html";
 }
