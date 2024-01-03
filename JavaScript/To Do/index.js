e1c2dad3b4e389fe02809699ad50eb23ab835887
Shushant Kumar <s.living.hell@gmail.com>
2024-01-03 12:32:50 +0530

    Adding completed to-watch app in JS

--- JavaScript/To Do/index.js
+++
@@ -1,9 +0,0 @@
-const addToCartBtn = document.getElementById("add-btn");
-const inputHolder = document.getElementById("input");
-const taskDisplay = document.getElementById("tasks");
-
-function addTask() {
-  taskDisplay.textContent += inputHolder.value + " ";
-}
-
-addToCartBtn.addEventListener("click", addTask);
